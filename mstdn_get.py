2b6fe53a6f067071ee6627a977c1ebb9da8bb92c
kelvin27315 <seven_units@yahoo.co.jp>
2020-11-01 01:04:08 +0900

    引数の順番を直した

--- mstdn_get.py
+++ mstdn_get.py
@@ -1,7 +1,7 @@
 from mstdn import Ponytail_Counter
 
 def main():
-    kasaki = Ponytail_Counter(id=22674, deltaday=1, url="https://example.com")
+    kasaki = Ponytail_Counter(id=22674, url="https://example.com", deltaday=1)
     kasaki.get_toots()
 
 
